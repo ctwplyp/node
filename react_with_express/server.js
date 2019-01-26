e3057ec63c2538a02c42a50e41336797eec00512
Sean <ctwplyp@gmail.com>
2019-01-26 15:40:44 -0800

    f/starting:  Added elastic search server

--- react_with_express/server.js
+++ react_with_express/server.js
@@ -5,7 +5,41 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+var elasticsearch = require('elasticsearch');
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+	
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+client.search
+({
+  index: 'bill',
+  type: '_doc',
+  body: {
+  	"_source": "items",
+    query: {
+      match: {
+        childId: 123456 
+      }
+    }
+  }
+}).then(function (resp) {
+    var hits = resp.hits.hits;
+    res.send({ express: hits });
+}, function (err) {
+    console.trace(err.message);
+});  
 });
